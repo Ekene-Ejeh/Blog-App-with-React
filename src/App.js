750053106e4fd873263496212396083b9a32fbfa
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-13 14:10:42 -0700

    Starts styling the TopBar conponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import TopBar from "./topbar/Topbar";
+import TopBar from "./topbar/TopBar";
 
 export default function App() {
 	return (
-		<div>
-			React Blog App
+		<>
 			<TopBar />
-		</div>
+		</>
 	);
 }
--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -1,6 +1,28 @@
 import React from "react";
 import "./topbar.css";
 
-export default function Topbar() {
-	return <div className="top">Topbar</div>;
+export default function TopBar() {
+	return (
+		<div className="top">
+			<div className="topLeft">
+				<i className="topIcon fa-brands fa-facebook-square"></i>
+				<i className="topIcon fa-brands fa-twitter-square"></i>
+				<i className="topIcon fa-brands fa-instagram-square"></i>
+				<i className="topIcon fa-brands fa-youtube-square"></i>
+			</div>
+			<div className="topCenter">
+				<ul className="topList">
+					<li className="topListItems">Home</li>
+					<li className="topListItems">About</li>
+					<li className="topListItems">Contact</li>
+					<li className="topListItems">Write</li>
+					<li className="topListItems">Logout</li>
+				</ul>
+			</div>
+			<div className="topRight">
+				<img className="topImg" src="" alt="" />
+				<i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+			</div>
+		</div>
+	);
 }
