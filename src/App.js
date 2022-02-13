89be0c116a65a2356a4ac37b79485cfcf272153d
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-13 07:20:55 -0700

    created TopBar component and its css styling file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from "react";
+import TopBar from "./topbar/Topbar";
 
 export default function App() {
-	return <div className="App">React Blog App</div>;
+	return (
+		<div>
+			React Blog App
+			<TopBar />
+		</div>
+	);
 }
