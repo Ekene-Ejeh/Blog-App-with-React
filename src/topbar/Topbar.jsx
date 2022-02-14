bb9ac2fcec38cdee41db9a51c0b626ee008eb103
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-14 10:08:35 -0700

    linked picture for profile pic on TopBar conponent and linked a picture on the Header component for the Home page

--- src/topbar/Topbar.jsx
+++ src/topbar/Topbar.jsx
@@ -20,7 +20,11 @@ export default function TopBar() {
 				</ul>
 			</div>
 			<div className="topRight">
-				<img className="topImg" src="" alt="" />
+				<img
+					className="topImg"
+					src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+					alt=""
+				/>
 				<i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
 			</div>
 		</div>
