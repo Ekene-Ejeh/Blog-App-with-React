ab0697a402f7b19b134e8eff4a5d92cc281530d4
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-16 10:00:06 -0700

    restructure the folders, moved all the components into a componenets folder and created a component called Single

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
-import TopBar from "./topbar/TopBar";
-import Home from "./pages/home/Home";
+import TopBar from "./components/topbar/TopBar";
+import Home from "./components/pages/home/Home";
+import Single from "./components/pages/single/Single";
 
 export default function App() {
 	return (
 		<>
 			<TopBar />
-			<Home />
+			<Single />
 		</>
 	);
 }
