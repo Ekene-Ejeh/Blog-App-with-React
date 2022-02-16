86cbeea63c4043f9f765b0b84ed9538e6496b9ea
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-16 08:59:43 -0700

    Done styling the Post conponent

--- src/post/Post.jsx
+++ src/post/Post.jsx
@@ -1,7 +1,44 @@
-import './post.css'
+import "./post.css";
 
 export default function Post() {
-  return (
-    <div>Post</div>
-  )
+	return (
+		<>
+			<div className="post">
+				<img
+					className="postImg"
+					src="https://themegoods-cdn-pzbycso8wng.stackpathdns.com/grandblog/demo/wp-content/uploads/2015/11/aboutme.jpg"
+					alt=""
+				/>
+				<div className="postInfo">
+					<div className="postCats">
+						<span className="postCat">Music</span>
+						<span className="postCat">Life</span>
+					</div>
+					<span className="postTitle">Lorem ipsum dolor sit</span>
+					<hr />
+					<span className="postDate"> 1 hour ago</span>
+				</div>
+				<div className="postDesc">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit
+					quia, aut quaerat tempora magnam nostrum nisi beatae dolore porro amet
+					eos. Qui, quos. Expedita suscipit consectetur quaerat itaque, laborum
+					doloribus ex blanditiis, ipsum, dignissimos nam nobis natus eligendi
+					illum culpa. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					Suscipit quia, aut quaerat tempora magnam nostrum nisi beatae dolore
+					porro amet eos. Qui, quos. Expedita suscipit consectetur quaerat
+					itaque, laborum doloribus ex blanditiis, ipsum, dignissimos nam nobis
+					natus eligendi illum culpa. Lorem ipsum dolor sit amet, consectetur
+					adipisicing elit. Suscipit quia, aut quaerat tempora magnam nostrum
+					nisi beatae dolore porro amet eos. Qui, quos. Expedita suscipit
+					consectetur quaerat itaque, laborum doloribus ex blanditiis, ipsum,
+					dignissimos nam nobis natus eligendi illum culpa. natus eligendi illum
+					culpa. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+					Suscipit quia, aut quaerat tempora magnam nostrum nisi beatae dolore
+					porro amet eos. Qui, quos. Expedita suscipit consectetur quaerat
+					itaque, laborum doloribus ex blanditiis, ipsum, dignissimos nam nobis
+					natus eligendi illum culpa.
+				</div>
+			</div>
+		</>
+	);
 }
