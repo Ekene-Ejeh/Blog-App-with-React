1851c83370bcc56808f37fe5931900f6989c1601
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-17 10:22:29 -0700

    starts styling the SinglePost component

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -3,7 +3,20 @@ import "./singlePost.css";
 export default function SinglePost() {
 	return (
 		<>
-			<div className="singlePost">SinglePost</div>
+			<div className="singlePost">
+				<div className="singlePostWrapper">
+					<img
+						src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+						alt=""
+						className="singlePostImg"
+					/>
+				</div>
+				<h1 className="singlePostTitle">Lorem ipsum dolor sit.</h1>
+				<div className="singlePostEdit">
+					<i class="fa-solid fa-pen-to-square"></i>
+					<i class="fa-solid fa-trash-can"></i>
+				</div>
+			</div>
 		</>
 	);
 }
