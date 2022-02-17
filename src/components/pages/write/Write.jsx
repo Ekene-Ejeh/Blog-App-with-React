f1f3da3a0c3db1486e0c153958293b24c8d94491
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-17 16:33:01 -0700

    Done styling the Write component

--- src/components/pages/write/Write.jsx
+++ src/components/pages/write/Write.jsx
@@ -4,10 +4,15 @@ export default function Write() {
 	return (
 		<>
 			<div className="write">
+				<img
+					className="writeImg"
+					src="https://images.pexels.com/photos/6685428/pexels-photo-6685428.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+					alt=""
+				/>
 				<form action="" className="writeForm">
 					<div className="writeFormGroup">
 						<label htmlFor="fileInput">
-							<i class="fa-solid fa-plus"></i>
+							<i className="writeIcon fa-solid fa-plus"></i>
 						</label>
 						<input type="file" id="fileInput" style={{ display: "none" }} />
 						<input
@@ -17,11 +22,11 @@ export default function Write() {
 							autoFocus={true}
 						/>
 					</div>
-					<div className="writeForGroup">
+					<div className="writeFormGroup ">
 						<textarea
-							placeholder="Tell your story.."
+							placeholder="Tell your story..."
 							type="text"
-							className="writeInput  writeText  "
+							className="writeInput  writeText "
 						></textarea>
 					</div>
 					<button className="writeSubmit">Publish</button>
