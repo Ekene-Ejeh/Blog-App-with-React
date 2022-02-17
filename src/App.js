70c4cb864e5c54b3d51b04472ec4d86fa86f4f54
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-17 12:41:01 -0700

    created a component called Write

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import TopBar from "./components/topbar/TopBar";
-// import Home from "./components/pages/home/Home";
+import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
+import Write from "./components/pages/write/Write";
 
 export default function App() {
 	return (
 		<>
 			<TopBar />
-			<Single />
+			<Write />
 		</>
 	);
 }
