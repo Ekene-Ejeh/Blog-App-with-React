e9bd3f98b7da1d13c29255d8a8d808e164e059a7
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-17 13:04:17 -0700

    starts styling the Write component

--- src/components/pages/write/Write.jsx
+++ src/components/pages/write/Write.jsx
@@ -3,7 +3,30 @@ import "./write.css";
 export default function Write() {
 	return (
 		<>
-			<div className="write">Write</div>
+			<div className="write">
+				<form action="" className="writeForm">
+					<div className="writeFormGroup">
+						<label htmlFor="fileInput">
+							<i class="fa-solid fa-plus"></i>
+						</label>
+						<input type="file" id="fileInput" style={{ display: "none" }} />
+						<input
+							type="text"
+							placeholder="Title"
+							className="writeInput"
+							autoFocus={true}
+						/>
+					</div>
+					<div className="writeForGroup">
+						<textarea
+							placeholder="Tell your story.."
+							type="text"
+							className="writeInput  writeText  "
+						></textarea>
+					</div>
+					<button className="writeSubmit">Publish</button>
+				</form>
+			</div>
 		</>
 	);
 }
