5bdcd7f1c83ff132c4a43c48b9804fdc0f8524a9
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-19 15:16:22 -0700

    Done styling the Login component

--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -5,11 +5,20 @@ export default function Login() {
 	return (
 		<>
 			<div className="login">
+				<span className="loginTitle">Login</span>
 				<form className="loginForm">
-					<label htmlFor="">Email</label>
-					<input type="text" placeholder="Enter tour email" />
-					<label htmlFor="">Password</label>
-					<input type="password" placeholder="Enter tour password" />
+					<label>Email</label>
+					<input
+						className="loginInput"
+						type="text"
+						placeholder="Enter your email"
+					/>
+					<label>Password</label>
+					<input
+						className="loginInput"
+						type="password"
+						placeholder="Enter your password"
+					/>
 					<button className="loginButton">Login</button>
 				</form>
 				<button className="loginRegisterButton">Register</button>
