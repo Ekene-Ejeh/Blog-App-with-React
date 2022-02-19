58398682cd74936941d01345cb97e9e77d905f39
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-18 21:46:35 -0700

    starts styling the Login component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import TopBar from "./components/topbar/TopBar";
 import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
-import Settings from "./components/pages/settings/Settings.jsx";
+import Settings from "./components/pages/settings/Settings";
+import Login from "./components/pages/login/Login.jsx";
 
 export default function App() {
 	return (
 		<>
 			<TopBar />
-			<Settings />
+			<Login />
 		</>
 	);
 }
--- src/components/pages/login/Login.jsx
+++ src/components/pages/login/Login.jsx
@@ -4,7 +4,16 @@ import "./login.css";
 export default function Login() {
 	return (
 		<>
-			<div>Login</div>
+			<div className="login">
+				<form className="loginForm">
+					<label htmlFor="">Email</label>
+					<input type="text" placeholder="Enter tour email" />
+					<label htmlFor="">Password</label>
+					<input type="password" placeholder="Enter tour password" />
+					<button className="loginButton">Login</button>
+				</form>
+				<button className="loginRegisterButton">Register</button>
+			</div>
 		</>
 	);
 }
