af2cb3747da6330c6a106bec748aee8984463d30
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-19 18:27:01 -0700

    installed react-router-dom. trying to link the TopBar menus to their respectie components but having issues with the router

--- src/App.js
+++ src/App.js
@@ -4,14 +4,31 @@ import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
 import Write from "./components/pages/write/Write";
 import Settings from "./components/pages/settings/Settings";
-import Login from "./components/pages/login/Login.jsx";
-import Register from "./components/pages/register/Register.jsx";
+import Login from "./components/pages/login/Login";
+import Register from "./components/pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 export default function App() {
 	return (
-		<>
+		<Router>
 			<TopBar />
-			<Register />
-		</>
+			<Switch>
+				<Route exact path="/">
+					<Home />
+				</Route>
+				<Route path="/register">
+					<Register />
+				</Route>
+				<Route path="/login">
+					<Login />
+				</Route>
+				<Route path="/write">
+					<Write />
+				</Route>
+				<Route path="/settings">
+					<Settings />
+				</Route>
+			</Switch>
+		</Router>
 	);
 }
