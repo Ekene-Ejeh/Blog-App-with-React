547c839603c34fb9d5c8877fd2ebee9d8010cd29
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-20 23:04:29 -0700

    did some restyling on some components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import TopBar from "./components/topbar/TopBar";
 import Home from "./components/pages/home/Home";
 import Single from "./components/pages/single/Single";
@@ -6,7 +7,7 @@ import Write from "./components/pages/write/Write";
 import Settings from "./components/pages/settings/Settings";
 import Login from "./components/pages/login/Login";
 import Register from "./components/pages/register/Register";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Post from "./components/post/Post";
 
 export default function App() {
 	return (
@@ -18,6 +19,7 @@ export default function App() {
 				<Route path="/login" element={<Login />} />
 				<Route path="/write" element={<Write />} />
 				<Route path="/settings" element={<Settings />} />
+				<Route path="/post/:postId" element={<Single />} />
 			</Routes>
 		</>
 	);
