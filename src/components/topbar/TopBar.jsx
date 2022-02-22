142b329655b22666f52266be276629c7755f3af8
Ekene Ejeh <ekeneejeh@gmail.com>
2022-02-21 22:13:43 -0700

    have linked the menus on the TopBar component and added some logic

--- src/components/topbar/TopBar.jsx
+++ src/components/topbar/TopBar.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import "./topbar.css";
 
 export default function TopBar() {
+	const user = false;
 	return (
 		<>
 			<div className="top">
@@ -15,20 +16,50 @@ export default function TopBar() {
 				<div className="topCenter">
 					<ul className="topList">
 						<li className="topListItems">
-							<Link to="/">HOME</Link>
+							<Link className="Link" to="/">
+								HOME
+							</Link>
 						</li>
-						<li className="topListItems">ABOUT</li>
-						<li className="topListItems">CONTACT</li>
-						<li className="topListItems">WRITE</li>
-						<li className="topListItems">LOGOUT</li>
+						<li className="topListItems">
+							<Link className="Link" to="/">
+								ABOUT
+							</Link>
+						</li>
+						<li className="topListItems">
+							<Link className="Link" to="/">
+								CONTACT
+							</Link>
+						</li>
+						<li className="topListItems">
+							<Link className="Link" to="/write">
+								WRITE
+							</Link>
+						</li>
+						<li className="topListItems">{user && "LOGOUT"}</li>
 					</ul>
 				</div>
 				<div className="topRight">
-					<img
-						className="topImg"
-						src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
-						alt=""
-					/>
+					{user ? (
+						<img
+							className="topImg"
+							src="https://images.pexels.com/photos/1858175/pexels-photo-1858175.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+							alt=""
+						/>
+					) : (
+						<ul className="topList">
+							<li className="topListItems">
+								<Link className="Link" to="/login">
+									LOGIN
+								</Link>
+							</li>
+							<li className="topListItems">
+								<Link className="Link" to="/register">
+									REGISTER
+								</Link>
+							</li>
+						</ul>
+					)}
+
 					<i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
 				</div>
 			</div>
